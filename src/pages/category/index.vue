6f6e39b261ada4bae1c1c7a885235e13e4f7bf53
likaifeng <1980942486@qq.com>
2018-12-23 14:13:45 +0800

    分类页修复 - 点击对应的菜单右侧渲染对应的内容

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -2,23 +2,28 @@
   <div class="category">
     <dd-search></dd-search>
     <div class="category-wrapper">
-      <div class="left-menu">
+      <scroll-view scroll-y class="left-menu">
         <ul ref="menuwrapper">
-          <li :class="index === 0?'menu-active':''" v-for="(menuItem, index) in menuLIst" :key="menuItem.cat_id">
+          <li 
+          :class="{'menu-active':index === currentIndex}"
+          v-for="(menuItem, index) in menuLIst" 
+          :key="menuItem.cat_id"
+          @click="handleClickMenu(index)"
+          >
             <span class="menu-text">{{menuItem.cat_name}}</span>
           </li>
         </ul>
-      </div>
-      <div class="right-content">
+      </scroll-view>
+      <scroll-view scroll-y class="right-content">
         <div class="content-wrapper">
-          <div class="content-list" v-for="(fitem, findex) in contentList" :key="findex">
+          <div class="content-list">
             <div class="banner"></div>
-            <div class="content" v-for="(sitem, sindex) in fitem.children" :key="sindex">
+            <div class="content" v-for="(sitem, sindex) in contentList.children" :key="sindex">
               <h3>{{sitem.cat_name}}</h3>
               <ul>
                 <li v-for="(sonList,sonIndex) in sitem.children" :key="sonIndex">
                   <navigator class="cate-list" hover-class="none">
-                    <img class="cat-icon" :src="'https://itjustfun.cn/'+sonList.cat_icon" :alt="sonList.cat_name">
+                    <img class="cat-icon" :src="'https://itjustfun.cn/'+sonList.cat_icon" :alt="sonList.cat_name" mode="aspectFill">
                     <span class="cat-name">{{sonList.cat_name}}</span>
                   </navigator>
                 </li>
@@ -26,31 +31,39 @@
             </div>
           </div>
         </div>
-      </div>
+      </scroll-view>
     </div>
   </div>
 </template>
 
 <script>
-import search from '@/components/search/search.vue'
+import search from '../../components/search/search.vue'
 export default {
   data () {
     return {
       menuLIst: [],
       active: true,
-      contentList: []
+      contentList: {},
+      currentIndex: 0
+    }
+  },
+  methods: {
+    handleClickMenu (index) {
+      this.currentIndex = index
+      this.contentList = this.menuLIst[index]
+      // console.log(this.contentList)
     }
   },
   mounted () {
     wx.request({
       url: 'https://itjustfun.cn/api/public/v1/categories',
       success: (res) => {
-        console.log(res)
+        // console.log(res)
         const {meta} = res.data
         if (meta.status === 200) {
           const {data} = res.data
           this.menuLIst = data
-          this.contentList = data
+          this.contentList = data[0]
         }
       }
     })

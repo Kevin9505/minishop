b3d3d538790ceafa0dc4bdd88f9fdc44929b78a3
likaifeng <1980942486@qq.com>
2018-12-26 09:10:04 +0800

    个人页面头部布局实现

--- src/pages/personal/index.vue
+++ src/pages/personal/index.vue
@@ -1,5 +1,35 @@
 <template>
-  <div>个人</div>
+  <div class="personal-wrapper">
+    <div class="userinfo-wrapper">
+      <div class="user-info">
+        <div class="setting iconfont icon-weibiaoti103"></div>
+        <div class="avator">
+        </div>
+        <div class="message iconfont icon-xiaoxi1"></div>
+      </div>
+      <div class="user-name">登录 / 注册</div>
+    </div>
+    <div class="content">
+      <div class="tool-wrapper">
+        <div class="tool-list">
+          <span class="count">0</span>
+          <span class="list-name">收藏的店铺</span>
+        </div>
+        <div class="tool-list">
+          <span class="count">0</span>
+          <span class="list-name">收藏的店铺</span>
+        </div>
+        <div class="tool-list">
+          <span class="count">0</span>
+          <span class="list-name">收藏的店铺</span>
+        </div>
+        <div class="tool-list">
+          <span class="count">0</span>
+          <span class="list-name">收藏的店铺</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,6 +37,6 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import 'style.scss';
 </style>
